a546484aca3a0ababe409a2e94fa0b9963df712a
DeanWalsh <deanochambers@gmail.com>
2022-07-15 21:23:10 +0100

    added functionality to player&computer choices, variable hierarchy, win/lose conditions and scores

--- rps.py
+++ rps.py
@@ -15,13 +15,53 @@
 
 import random
 
-r = 1
-p = 2
-s = 3
-
 player_wins = 0
 computer_wins = 0
 
 print("Welcome to Rock, Paper, Scissors!")
-print("Type 'r' for rock, 'p' for paper, or 's' for scissors.")
-choice = input()
+
+while True:
+
+    print("Type 'r' for rock, 'p' for paper, or 's' for scissors.")
+
+    choice = input()
+    if choice == "r":
+        player_choice = 1
+    elif choice == "p":
+        player_choice = 2
+    elif choice == "s":
+        player_choice = 3
+    computer_choice = random.randint(1, 3)
+
+    if player_choice == computer_choice:
+        print("It's a tie!")
+    elif player_choice == 1 and computer_choice == 3:
+        print("You win!")
+        player_wins += 1
+    elif player_choice == 2 and computer_choice == 1:
+        print("You win!")
+        player_wins += 1
+    elif player_choice == 3 and computer_choice == 2:
+        print("You win!")
+        player_wins += 1
+    elif player_choice == 1 and computer_choice == 2:
+        print("You lose!")
+        computer_wins += 1
+    elif player_choice == 2 and computer_choice == 3:
+        print("You lose!")
+        computer_wins += 1
+    elif player_choice == 3 and computer_choice == 1:
+        print("You lose!")
+        computer_wins += 1
+
+    print("Player: " + str(player_wins))
+    print("Computer: " + str(computer_wins))
+    
+    print("")
+    print("Type 'q' to quit, or anything else to play again.")
+
+    answer = input()
+    if answer == "q":
+        break
+    else:
+        continue
